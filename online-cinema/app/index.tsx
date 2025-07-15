a1eb3182a1ffbcc9e1e570c1cc509aa6e28f3ed2
boytrend0108 <boytrend@gmail.com>
2025-07-15 09:09:57 +0300

    feat: create an empty project

---
+++ online-cinema/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>Hello bro!!! you are amazing!!! Keep going!</Text>
+    </View>
+  );
+}
