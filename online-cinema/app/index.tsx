2735f472837ec0e50384ca9270025bf740d0bffe
boytrend0108 <boytrend@gmail.com>
2025-07-16 08:29:02 +0300

    feat: add routing

--- online-cinema/app/index.tsx
+++ online-cinema/app/index.tsx
@@ -1,11 +1,15 @@
+import { Link } from 'expo-router';
 import { Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View className='flex-1 items-end justify-center bg-gray-200'>
-      <Text className='text-accent text-3xl font-bold'>
-        Hello bro!!! you are amazing!!! Keep going!
-      </Text>
+    <View className='flex-1 items-center justify-center bg-gray-200'>
+      <Text className='text- text-3xl font-bold'>Hello bro!!!</Text>
+
+      <Link href='/onboarding'>Onbording</Link>
+      <Link href={{ pathname: '/movie/[id]', params: { id: 'avengers' } }}>
+        Avengers Movie
+      </Link>
     </View>
   );
 }
