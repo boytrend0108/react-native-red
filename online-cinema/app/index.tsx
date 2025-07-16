51c308dc90c55b90489e2c3e4d3f892804c1ebd7
boytrend0108 <boytrend@gmail.com>
2025-07-16 08:10:11 +0300

    feat:add nativewind

--- online-cinema/app/index.tsx
+++ online-cinema/app/index.tsx
@@ -2,14 +2,10 @@ import { Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Hello bro!!! you are amazing!!! Keep going!</Text>
+    <View className='flex-1 items-end justify-center bg-gray-200'>
+      <Text className='text-accent text-3xl font-bold'>
+        Hello bro!!! you are amazing!!! Keep going!
+      </Text>
     </View>
   );
 }
